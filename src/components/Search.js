9c8b90fedca0472a331cd6432b9bf75cda2c2503
fstawicki <stawickifilip1@gmail.com>
2022-03-21 19:50:55 +0100

    working on connecting to API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {useState} from 'react';
 import styles from './Search.module.css';
 
 function Search() {
+
+  const [searchbar,setSearchbar] = useState('');
+
+  const searchbarHandler = (e) => {
+    setSearchbar(e.target.value);
+    console.log(searchbar)
+  }
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+
+    console.log(searchbar);
+    setSearchbar('');
+  }
+
   return (
     <div className={styles.search}>
-        <form className={styles.search__form}>
-        <input type="text" className={styles.search__searchbar} />
+        <form className={styles.search__form} onSubmit={submitHandler}>
+        <input type="text" className={styles.search__searchbar} onChange={searchbarHandler} value={searchbar} />
         <button type="submit" className={styles.search__searchbtn}>Search</button>
         </form>
 
