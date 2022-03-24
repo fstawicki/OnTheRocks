514457c4ac7a4ad6d85ede83a5b43627f3b3da75
fstawicki <stawickifilip1@gmail.com>
2022-03-24 23:22:28 +0100

    working on displaying data - passed props to parent

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,81 +2,30 @@ import React, { Fragment } from 'react';
 import {useState} from 'react';
 import styles from './Search.module.css';
 
-function Search() {
+function Search(props) {
 
   const [searchbar,setSearchbar] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [drinks, setDrinks] = useState([]);
+  let drinksArray = [];
 
 
   const searchbarHandler = (e) => {
     setSearchbar(e.target.value);
   }
 
-  const submitHandler = (e) => {
+
+  async function submitHandler(e){
     e.preventDefault();
-    if(searchbar === '' || searchbar === null){
-      alert('enter drink name');
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchbar}`);
+    if(response.status !== 200){
+      console.log('nie');
       return;
     }
-    
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchbar}`).then(response => {
-        if(response.status !== 200){
-          console.log('spierdolone');
-          return;
-        }
-        return response.json();
-      }).then(data => {
-        const resultArray = data.drinks.map(item => {
-          // return console.log(item.strDrink);
-          return setDrinks({
-            key: item.strDrink,
-            name: item.strDrink,
-            image: item.strDrinkThumb,
-            category: item.strCategory,
-            glass: item.strGlass,
-            ing1: item.strIngredient1,
-            ing2: item.strIngredient2,
-            ing3: item.strIngredient3,
-            ing4: item.strIngredient4,
-            ing5: item.strIngredient5,
-            ing6: item.strIngredient6,
-            ing7: item.strIngredient7,
-            ing8: item.strIngredient8,
-            ing9: item.strIngredient9,
-            ing10: item.strIngredient10,
-            ing11: item.strIngredient11,
-            ing12: item.strIngredient12,
-            ing13: item.strIngredient13,
-            ing14: item.strIngredient14,
-            ing15: item.strIngredient15,
-            instruction: item.strInstructions,
-            measure1: item.strMeasure1,
-            measure2: item.strMeasure2,
-            measure3: item.strMeasure3,
-            measure4: item.strMeasure4,
-            measure5: item.strMeasure5,
-            measure6: item.strMeasure6,
-            measure7: item.strMeasure7,
-            measure8: item.strMeasure8,
-            measure9: item.strMeasure9,
-            measure10: item.strMeasure10,
-            measure11: item.strMeasure11,
-            measure12: item.strMeasure12,
-            measure13: item.strMeasure13,
-            measure14: item.strMeasure14,
-            measure15: item.strMeasure15,
-          })
-        })
-      }).catch(error => {
-        console.error(error);
-      });
-
-      console.log(drinks);
-      setIsLoading(false);
-
-    setSearchbar('');
-    
+      const data = await response.json();
+      drinksArray = data.drinks;
+      
+  props.sendDrinks(drinksArray);
+      
   }
 
 
@@ -96,3 +45,71 @@ function Search() {
 }
 
 export default Search;
+
+
+
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
+  //   if(searchbar === '' || searchbar === null){
+  //     alert('enter drink name');
+  //     return;
+  //   }
+    
+  //   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchbar}`).then(response => {
+  //       if(response.status !== 200){
+  //         console.log('spierdolone');
+  //         return;
+  //       }
+  //       return response.json();
+  //     }).then(data => {
+  //       const resultArray = data.drinks.map(item => {
+  //         // return console.log(item.strDrink);
+  //         return setDrinks({
+  //           key: item.strDrink,
+  //           name: item.strDrink,
+  //           image: item.strDrinkThumb,
+  //           category: item.strCategory,
+  //           glass: item.strGlass,
+  //           ing1: item.strIngredient1,
+  //           ing2: item.strIngredient2,
+  //           ing3: item.strIngredient3,
+  //           ing4: item.strIngredient4,
+  //           ing5: item.strIngredient5,
+  //           ing6: item.strIngredient6,
+  //           ing7: item.strIngredient7,
+  //           ing8: item.strIngredient8,
+  //           ing9: item.strIngredient9,
+  //           ing10: item.strIngredient10,
+  //           ing11: item.strIngredient11,
+  //           ing12: item.strIngredient12,
+  //           ing13: item.strIngredient13,
+  //           ing14: item.strIngredient14,
+  //           ing15: item.strIngredient15,
+  //           instruction: item.strInstructions,
+  //           measure1: item.strMeasure1,
+  //           measure2: item.strMeasure2,
+  //           measure3: item.strMeasure3,
+  //           measure4: item.strMeasure4,
+  //           measure5: item.strMeasure5,
+  //           measure6: item.strMeasure6,
+  //           measure7: item.strMeasure7,
+  //           measure8: item.strMeasure8,
+  //           measure9: item.strMeasure9,
+  //           measure10: item.strMeasure10,
+  //           measure11: item.strMeasure11,
+  //           measure12: item.strMeasure12,
+  //           measure13: item.strMeasure13,
+  //           measure14: item.strMeasure14,
+  //           measure15: item.strMeasure15,
+  //         })
+  //       })
+  //     }).catch(error => {
+  //       console.error(error);
+  //     });
+
+  //     console.log(drinks);
+  //     setIsLoading(false);
+
+  //   setSearchbar('');
+    
+  // }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Cocktail from '../components/Cocktail';
 import Search from '../components/Search';
 
 import styles from './MainPage.module.css'
 
 function MainPage() {
+
+  const getDrinks = (drinksData) => {
+    const drinks = [...drinksData];
+    console.log(drinks);
+  }
+
   return (
     <div className={styles.main}>
-      <Search />
+      <Search sendDrinks={getDrinks} />
       <Cocktail />
       <Cocktail />
       <Cocktail />
